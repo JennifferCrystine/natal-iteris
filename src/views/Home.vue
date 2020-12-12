4a0552d4bb6c2d980dc0dc7a24018367bdc15db2
Patricia Faustino <patriciafaustinobr@gmail.com>
2020-12-12 15:27:19 -0300

    Atualizei view Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,30 @@
 <template>
   <v-container class="home d-flex flex-column align-center fill-height mt-5">
+  
     <h1 class="text-h4 text-center">Natal Solidário</h1>
-    <p class="text-justify mt-5">
-      O natal este ano será diferenciado.
-       Papai Noel - assim como todos nós - está
-        se cuidando nesta pandemia e não poderá visitar de casa em casa
-         para entregar os presentes. Mas e agora, as crianças ficarão sem seus
-          presentinhos de natal? 😱 Não, os duendes atualizaram o papai Noel com
-          o aplicativo. Você escolhe uma cartinha e ajuda uma criança no processo.
-    </p>
+    <div class="texto-imagem d-flex flex-column align-center justify-space-between">
+      <p class="text-justify mt-5">
+        O natal este ano será diferenciado.
+        Papai Noel - assim como todos nós - está
+          se cuidando nesta pandemia e não poderá visitar de casa em casa
+          para entregar os presentes. Mas e agora, as crianças ficarão sem seus
+            presentinhos de natal? 😱 Não, os duendes atualizaram o papai Noel com
+            o aplicativo. Você escolhe uma cartinha e ajuda uma criança no processo.
+      </p>
 
-    <v-img
-      alt="Papai Noel"
-      max-height="150"
-      max-width="250"
-      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtVdbefQZWhg9ue0VOJFgDl0EtJTT8i67yA&usqp=CAU"
-    ></v-img>
-    <BotaoPrimeiraPagina/>
+    
+      <v-img 
+        alt="Papai Noel"
+        max-height="150"
+        max-width="250"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtVdbefQZWhg9ue0VOJFgDl0EtJTT8i67yA&usqp=CAU"
+      ></v-img>
+
+    </div>
+
+    <v-btn class="signin mt-2" x-large rounded to ="/letter" color="green darken-4">
+        <v-icon>mdi-pine-tree</v-icon> <span class="">Doe aqui</span>
+    </v-btn>
   </v-container>
 
   
@@ -24,13 +32,11 @@
 
 <script>
 
-import BotaoPrimeiraPagina from '@/components/shared/BotaoPrimeiraPagina'
 
 export default {
   name: 'Home',
   components: {
-    BotaoPrimeiraPagina,
-    
+
     
   }
 }
