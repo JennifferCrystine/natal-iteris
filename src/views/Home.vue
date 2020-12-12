ee9d949d52f84c8049f007e0bdb8501db9e2509e
Jenniffer Crystine <jenniffercrystine@gmail.com>
2020-12-12 10:57:27 -0300

    inicia tela principal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home text-center d-flex flex-column align-center justify-">
+    <p>Jujubes jelly-o gingerbread caramels cookie soufflé gingerbread chocolate cake. Gummi bears sweet roll cake toffee jujubes pie topping carrot cake. Biscuit icing danish candy pie cotton candy cake jujubes cookie. Oat cake wafer sweet roll cotton candy liquorice cake wafer caramels gummi bears.
+    Pie dessert chocolate muffin cheesecake. Tootsie roll dragée cotton candy pie sugar plum chupa chups jujubes sugar plum. Tart caramels sugar plum oat cake soufflé. Pudding gingerbread topping tootsie roll danish.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
