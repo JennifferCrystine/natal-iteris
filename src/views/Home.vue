c9e4db13768e8ff67066b96ff8fee248603e889e
Jenniffer Crystine <jenniffercrystine@gmail.com>
2020-12-12 12:53:12 -0300

    estilo aplicado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="home d-flex flex-column align-center justify-center">
-    <p class="text-center">Jujubes jelly-o gingerbread caramels cookie soufflé gingerbread chocolate cake. Gummi bears sweet roll cake toffee jujubes pie topping carrot cake. Biscuit icing danish candy pie cotton candy cake jujubes cookie. Oat cake wafer sweet roll cotton candy liquorice cake wafer caramels gummi bears.
-    Pie dessert chocolate muffin cheesecake. Tootsie roll dragée cotton candy pie sugar plum chupa chups jujubes sugar plum. Tart caramels sugar plum oat cake soufflé. Pudding gingerbread topping tootsie roll danish.</p>
-    
-  </div>
+  <v-container class="home d-flex flex-column align-center fill-height mt-5">
+    <h1 class="text-h4 text-center">Natal Solidário</h1>
+    <p class="text-center mt-5">
+      Jujubes jelly-o gingerbread caramels cookie soufflé gingerbread chocolate
+      cake. Gummi bears sweet roll cake toffee jujubes pie topping carrot cake.
+      Biscuit icing danish candy pie cotton candy cake jujubes cookie. Oat cake
+      wafer sweet roll cotton candy liquorice cake wafer caramels gummi bears.
+      Pie dessert chocolate muffin cheesecake. Tootsie roll dragée cotton candy
+      pie sugar plum chupa chups jujubes sugar plum. Tart caramels sugar plum
+      oat cake soufflé. Pudding gingerbread topping tootsie roll danish.
+    </p>
+  </v-container>
 </template>
 
 <script>
-
 export default {
-  name: 'Home',
-}
+  name: "Home",
+};
 </script>
