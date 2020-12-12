f62020f0272d95d83abf5cd5453a2b33ccb04487
Luiza Naves <luizannaves@icloud.com>
2020-12-12 12:59:51 -0400

    Rotas e barra

--- src/components/shared/AppBarraLateral.vue
+++ src/components/shared/AppBarraLateral.vue
@@ -1,51 +1,54 @@
 <template>
-  <v-card>
+  <v-div>
+    <v-app-bar color="red darken-4" dark app>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Be zen</v-toolbar-title>
+    </v-app-bar>
+
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant.sync="mini"
-      permanent
-      color="red"
+      absolute
+      temporary
     >
-      <v-list-item class="px-2">
-        <v-list-item-avatar>
-          <v-img src=""></v-img>
-        </v-list-item-avatar>
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="red darken-4">
 
-        <v-list-item-title>Natal Solidario</v-list-item-title>
-
-        <v-btn
-          icon
-          @click.stop="mini = !mini"
-        >
-          <v-icon>mdi-chevron-left</v-icon>
-        </v-btn>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense app >
-        
           <router-link to="/home">
-          <v-list-item>
+          <v-list-item class="1">
             <v-list-item-icon>
               <v-icon>mdi-home</v-icon>
             </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
+            <v-list-item-title>Incio</v-list-item-title>
           </v-list-item>
-        </router-link>
+          </router-link>
 
+          <router-link to="cartas">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon><mdi-email-letter></mdi-email-letter></v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Cartinhas</v-list-item-title>
+          </v-list-item>
+          </router-link>
+
+        </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
-  </v-card>
+  </v-div>
 </template>
 
 <script>
   export default {
-    data () {
-      return {
-        drawer: true,
-        mini: true,
-      }
-    },
-  }
+    name: 'AppBarraNavegacao',
+    data: () => ({
+      drawer: false,
+      group: null,
+    }),
+};
 </script>
