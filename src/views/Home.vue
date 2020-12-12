2586c94676eee552e4cfb80cc3d478732c41323b
Jenniffer Crystine <jenniffercrystine@gmail.com>
2020-12-12 11:23:05 -0300

    rascunho tela inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="home text-center d-flex flex-column align-center justify-">
-    <p>Jujubes jelly-o gingerbread caramels cookie soufflé gingerbread chocolate cake. Gummi bears sweet roll cake toffee jujubes pie topping carrot cake. Biscuit icing danish candy pie cotton candy cake jujubes cookie. Oat cake wafer sweet roll cotton candy liquorice cake wafer caramels gummi bears.
+  <div class="home d-flex flex-column align-center justify-center">
+    <p class="text-center">Jujubes jelly-o gingerbread caramels cookie soufflé gingerbread chocolate cake. Gummi bears sweet roll cake toffee jujubes pie topping carrot cake. Biscuit icing danish candy pie cotton candy cake jujubes cookie. Oat cake wafer sweet roll cotton candy liquorice cake wafer caramels gummi bears.
     Pie dessert chocolate muffin cheesecake. Tootsie roll dragée cotton candy pie sugar plum chupa chups jujubes sugar plum. Tart caramels sugar plum oat cake soufflé. Pudding gingerbread topping tootsie roll danish.</p>
+    
   </div>
 </template>
 
