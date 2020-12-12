a33de7bdce3a619a11d1036284f02ccf07976ce5
Patricia Faustino <patriciafaustinobr@gmail.com>
2020-12-12 12:55:40 -0300

    Criei arqivo BotaoPrimeiraPagina

---
+++ src/components/shared/BotaoPrimeiraPagina.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="text-center">
+        
+            <v-btn
+              class="success"
+              x-large
+              dark
+
+          
+              
+            >
+            
+              <v-icon>mdi-pine-tree</v-icon> <span >Doe aqui</span>
+            </v-btn>
+    
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BotaoPrimeiraPagina'
+}
+</script>
+<style scoped>
+.botao{
+    background-color: green;
+    
+}
+</style>
